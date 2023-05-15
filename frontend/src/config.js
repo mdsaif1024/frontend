b0356ab541b141f80263844a143674213e323acc
mdsaif1024 <mdsaif1024@gmail.com>
2023-05-15 20:35:43 +0530

    added dimensions

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,13 +6,25 @@ const app_config = {
               name : 'Facebook Mobile Post',
               category : 'social-media',
               type: 'mobile',
-                image : '/mockups/img6.jpg'
+                image : '/mockups/img6.jpg',
+                dimensions: {
+                  x: 474,
+                  y: 196,
+                  width: 338,
+                  height: 490
+                }
             },
             {
               name : 'Instagram Mobile Post',
               category : 'social-media',
               type: 'mobile',
-                image : '/mockups/img9.jpg'
+                image : '/mockups/img9.jpg',
+                dimensions: {
+                  x: 268,
+                  y: 538,
+                  width: 375,
+                  height: 271
+                }
             },
             {
               name : 'Facebook Website Post',
@@ -24,7 +36,13 @@ const app_config = {
               name : 'Instagram Website Post',
               category : 'social-media',
               type: 'desktop',
-                image : '/mockups/img3.jpg'
+                image : '/mockups/img3.jpg',
+                dimensions: {
+                  x: 414,
+                  y: 273,
+                  width: 502,
+                  height: 278
+                }
             },
             {
               name : 'Instagram Website Post',
@@ -36,22 +54,41 @@ const app_config = {
               name : 'Instagram Website Post',
               category : 'social-media',
               type: 'desktop',
-                image : '/mockups/img5.jpg'
+                image : '/mockups/img5.jpg',
+                dimensions: {
+                  x: 549,
+                  y: 424,
+                  width: 168,
+                  height: 149
+                }
             },
             {
               name : 'Instagram Website Post',
               category : 'social-media',
               type: 'desktop',
-                image : '/mockups/img11.jpg'
+                image : '/mockups/img11.jpg',
+                dimensions: {
+                  x: 402,
+                  y: 268,
+                  width: 364,
+                  height: 203
+                }
             },
             {
               name : 'Instagram Website Post',
               category : 'social-media',
               type: 'desktop',
-                image : '/mockups/img12.jpg'
+                image : '/mockups/img12.jpg',
+                dimensions: {
+                  x: 506,
+                  y: 198,
+                  width: 268,
+                  height: 518
+                }
             },
             
-        ]
+        ],
+
     }
 }
 
